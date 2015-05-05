5ce70e12a3c3de4116d8996ae7287815214cac5d
chhuang <chhuang@live.cn>
2015-05-05 17:02:00 +0800

    Add some basic parser for Json

--- JsonParser/Parser.cs
+++ JsonParser/Parser.cs
@@ -1,13 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Parsec;
+using Parsec.Core;
 
 namespace JsonParser
 {
-    public static class Parser
+    public class JObject
     {
+    }
 
+    public class JString : JObject
+    {
+    }
+
+    public static class Deserializer
+    {
+        public static Parser<Char, Char> Quote = Chars.One('"');
+        public static Parser<Char, Char> Slash = Chars.One('\\');
+        public static Parser<Char, Char> QuotationMark = Slash.And(Quote, (left, right) => right);
+        public static Parser<Char, Char> SlashMark = Slash.And(Slash, (left, right) => right);
+        public static Parser<Char, Char> ReverseSlashMark = Slash.And(Chars.One('/'), (left, right) => right);
+        public static Parser<Char, Char> BackSpaceMark = Slash.And(Chars.One('b'), (left, right) => '\b');
+        public static Parser<Char, Char> FormfeedMark = Slash.And(Chars.One('f'), (left, right) => '\f');
+        public static Parser<Char, Char> NewLineMark = Slash.And(Chars.One('n'), (left, right) => '\n');
+        public static Parser<Char, Char> CarriageReturnMark = Slash.And(Chars.One('r'), (left, right) => '\r');
+        public static Parser<Char, Char> TabMark = Slash.And(Chars.One('t'), (left, right) => '\t');
+        public static Parser<Char, Char> Unicode = Slash.And(
+            Chars.One('u').And(Chars.HexDigit().RepeatN(4)),
+            (_, digits) => (Char)Int32.Parse(new String(digits), NumberStyles.HexNumber));
     }
 }
